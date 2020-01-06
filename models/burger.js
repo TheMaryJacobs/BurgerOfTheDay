7f20b0417effc5533517382f973769d32146dc7a
Mary Jacobs <TheMaryJacobs@gmail.com>
2020-01-06 16:53:55 -0600

    models setup

--- models/burger.js
+++ models/burger.js
@@ -4,3 +4,29 @@ const orm = require ("orm.js");
 // the ORM functions using burger specific input for the ORM.
 
 //  * Export at the end of the `burger.js` file.
+
+const Sequelize = require('sequelize')
+const Model = Sequelize.Model;
+
+module.exports = (sequelize, DataTypes) => {
+  class Burgers extends Model {}
+  Burgers.init({
+    // attributes
+    burger_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    devoured: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+      allowNull: false
+      // allowNull defaults to true
+    }
+  }, {
+      sequelize,
+      modelName: 'Burgers'
+      // options
+  });
+  Burgers.sync();
+  return Burgers;
+};
