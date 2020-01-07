4f10b376aaa09c4a672bd0740d77eb0d1c6318da
Mary Jacobs <TheMaryJacobs@gmail.com>
2020-01-06 18:08:33 -0600

    server.js & index.js

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
-const express = require ("express");
+const db = require("./models");
+const express = require("express");
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+// =============================================================
+app.use(express.static('public'))
+require("./routes/api-routes")(app);
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
+
+console.log ("hi");
